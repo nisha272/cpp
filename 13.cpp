bdbefcd5bd1edcb302c84d09226b22c11f7863d1
nisha272 <nisharana8077@gmail.com>
2026-03-27 19:39:27 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct Student {
+    int rollNumber;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    Student s;
+
+    cout << "Enter Roll Number: ";
+    cin >> s.rollNumber;
+
+    cout << "Enter Name: ";
+    getline(cin, s.name);
+
+    cout << "Enter Marks: ";
+    cin >> s.marks;
+
+  
+    cout << "\n Student Details" << endl;
+    cout << "Roll Number: " << s.rollNumber << endl;
+    cout << "Name:" << s.name << endl;
+    cout << "Marks:" << s.marks << endl;
+
+    return 0;
+}
