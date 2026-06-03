e0ca4f4a520aa7b9444e4403e9e76d173815874f
nisha272 <nisharana8077@gmail.com>
2026-06-03 17:27:17 +0530

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void pattern1(int n) {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 1; j <=i; j++) {
+                cout << j;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int n;
+
+    cout << "Enter the number: ";
+    cin >> n;
+
+    Solution s;      
+    s.pattern1(n);   
+
+    return 0;
+}
